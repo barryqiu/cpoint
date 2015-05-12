1dcdeeb91fa2b71ebdf295a2f123bf21e0acc3f6
barryqiu <qiuxiaobing90@126.com>
2015-05-11 18:36:29 -0700

    0.c

--- 0.c
+++ 0.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-/*
+
 	int poge = 5;
 	int * p_poge = &poge;
 	printf("%p\n",p_poge);
 	p_poge++;
 	printf("%p\n",p_poge);
 	printf("%p\n",p_poge+3);
-	return 0;
-*/
+
 	int a[5] = {1,2,3,4,5};
 	printf("%d\n", 3[a]);
 	
+	return 0;
 }
